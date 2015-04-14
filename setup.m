908e9df541b31ad9cac8660c60f2e6410852d01b
Scott Haile Robertson <ScottHaileRobertson@gmail.com>
2015-04-14 12:32:48 -0400

    Added comments for install, and message upon completion

--- setup.m
+++ setup.m
@@ -1,3 +1,8 @@
+% To get this setup to run, you need a mex compiler:
+%     In Linux:
+%         edit mexopts.sh to have -sdt=c99
+%         also line lstdc by http://stackoverflow.com/questions/17000903/mex-compiling-on-64-bit-linux-usr-bin-ld-cannot-find-lstdc
+
 % Get current path
 rootDistribDir = pwd();
 
@@ -8,3 +13,4 @@ path(genpath([rootDistribDir filesep() 'ReconPackage']),path);
 % Compiling MEX code
 disp('Compiling MEX code...');
 Recon.SysModel.Proximity.mex_compile_proximity();
+disp('Compilation worked');
