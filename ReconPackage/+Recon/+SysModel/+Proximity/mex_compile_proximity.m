a6177afd1af4e6e36c6682b6f3426e27234b98e4
James <jamesjcook@users.noreply.github.com>
2017-02-13 15:34:00 -0500

    Updated compile to not recompile all the time, and to use best practices for file paths.

--- ReconPackage/+Recon/+SysModel/+Proximity/mex_compile_proximity.m
+++ ReconPackage/+Recon/+SysModel/+Proximity/mex_compile_proximity.m
@@ -1,12 +1,29 @@
-% Get the starting directory
+% Gvet the starting directory
 rootDistribDir = pwd();
 
 % Change to the mex directory
-cd([rootDistribDir filesep() 'ReconPackage' filesep()...
-    '+Recon' filesep() '+SysModel' filesep() '+Proximity' filesep()]);
+cd(fullfile(rootDistribDir , 'ReconPackage' ,...
+    '+Recon' , '+SysModel' , '+Proximity' ));
 
 % Compile mex code
-mex -g sparse_gridding_distance_mex.c;
-
-% Change back to starting directory 
+do_compile=1;
+if exist('sparse_gridding_distance_mex.mexa64','file')
+    f1=dir('sparse_gridding_distance_mex.c');
+    f2=dir('sparse_gridding_distance.c');
+    fo=dir('sparse_gridding_distance_mex.mexa64');
+    if f1.datenum < fo.datenum ...
+            && f2.datenum < fo.datenum
+        do_compile=0;
+    end
+end
+if do_compile
+    try
+        mex -largeArrayDims sparse_gridding_distance_mex.c;
+    catch err
+        warning(err.message);
+        mex -n  -largeArrayDims sparse_gridding_distance_mex.c;
+        db_inplace(mfilename,'COMPILER PROBLEMS, We ran in test mode, use terminal commands preceding this line as examples to compile manually. Of note, -ansi may cause trouble on linux systems.');
+    end
+end
+% Change back to starting directory
 cd(rootDistribDir);
